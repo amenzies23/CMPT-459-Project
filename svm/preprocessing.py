dce0d29d6ebc46161987029b4dfd19d1bd119429
ivonr7 <imv@sfu.ca>
2025-10-10 21:13:11 -0700

    Added preprocessing for SVM

---
+++ svm/preprocessing.py
@@ -0,0 +1,53 @@
+# Isaac von Riedemann
+# 301423851
+# Each function should operate on a dataframe and any params
+# All functions should be chained together using pipe
+import marimo as mo
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+'''
+    Get Class Labels from Dataset (y)
+'''
+def get_labels(df):
+    return df.loc[:,["Plant_Health_Status"]]
+
+'''
+    Get All Features from the dataset (X)
+'''
+def get_features(df):
+    return df.drop(columns = ["Timestamp", "Plant_ID","Plant_Health_Status"])
+
+'''
+    Normalize all features for preparing for model
+'''
+def normalize(df):
+    scaler = StandardScaler()
+    scaled = scaler.fit_transform(df)
+    return pd.DataFrame(data=scaled,index=df.index, columns=df.columns)
+
+'''
+    Extract Informative features for Model
+'''
+def extract_features(df, attr:list):
+    assert len(attr) > 0
+    return df.loc[:, attr]
+
+'''
+    Preprocessing recipe to use
+'''
+def preprocess(df, attr:list = ["Soil_Moisture","Nitrogen_Level","Soil_pH"]):
+    return (
+        df
+        .pipe(get_features)
+        .pipe(extract_features,attr = attr)
+        .pipe(normalize)
+    )
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("../data/plant_health_data.csv")
+    model_ready = preprocess(df)
+    print(model_ready.head())
